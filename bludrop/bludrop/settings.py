babaf4b72172e92b237a28c24a9e8308031580e0
Enyene Akpan <akpanenyene@gmail.com>
2022-06-30 12:16:47 +0100

    define models and views add configurations for media files define urls mapping update models for admin dashboard

--- bludrop/bludrop/settings.py
+++ bludrop/bludrop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'photoapp'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = BASE_DIR/'static'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR/'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
