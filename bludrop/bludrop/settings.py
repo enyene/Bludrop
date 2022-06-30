6fc899394aa1ba94a3fd8b942eaa816ef69d6fed
Enyene Akpan <akpanenyene@gmail.com>
2022-06-30 13:06:35 +0100

    add templates and configurations

--- bludrop/bludrop/settings.py
+++ bludrop/bludrop/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIR = BASE_DIR/'static'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'static'
+LOGIN_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- bludrop/photoapp/views.py
+++ bludrop/photoapp/views.py
@@ -1,6 +1,27 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     return HttpResponse('this is the home page')
+
+def signup(request):
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+
+            user = authenticate(username= username,password= password)
+            login(request,user)
+
+            context = {'form':form}
+
+    return render (request,'photoapp/signup.html',context)
