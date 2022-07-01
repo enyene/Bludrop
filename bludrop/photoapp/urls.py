5d76a09845f62532ead0aa7d4cf394db8d486b26
Enyene Akpan <akpanenyene@gmail.com>
2022-07-01 11:39:24 +0100

    update on signup views

--- bludrop/photoapp/urls.py
+++ bludrop/photoapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name = 'home'),
-    path('signup/',views.signup,name = 'signup')
+    path('signup/',views.signup,name = 'signup'),
+    path('login/',views.login,name='login')
 ]
--- bludrop/photoapp/views.py
+++ bludrop/photoapp/views.py
@@ -5,9 +5,9 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required
+
 def index(request):
-    return HttpResponse('this is the home page')
+    return render(request,'photoapp/index.html')
 
 def signup(request):
     
@@ -21,7 +21,28 @@ def signup(request):
 
             user = authenticate(username= username,password= password)
             login(request,user)
+            return redirect('home')
 
-            context = {'form':form}
+        else:
+            form = UserCreationForm()
+            return render(request,'photoapp/signup.html',{'form':form})
 
-    return render (request,'photoapp/signup.html',context)
+    else:
+        form = UserCreationForm()
+        return render(request,'photoapp/signup.html',{'form':form})
+
+def login(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username= username,password= password)
+            login(request,user)
+            return redirect('home')
+        else:
+            form = UserCreationForm()
+            return render(request,'photoapp/signup.html',{'form':form})
+    else:
+         form = UserCreationForm()
+         return render(request,'photoapp/signup.html',{'form':form})
